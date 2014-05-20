7accbc6ce72b795206142b11c412cfc1910b1ce9
Matthew Bunday <zenceph@gmail.com>
2014-05-20 09:49:03 -0400

    Working on Controller and Views.

--- gmail.rb
+++ gmail.rb
@@ -1,16 +1,30 @@
 require 'highline'
 require 'gmail'
 
-class LoginView
-  def self.display
-    username = 
+module Views
+  def self.login
+    username = ask("Username: ")
+    password = ask("Password: ") { |q| q.echo = '*' }
+    Controller.authenticate(username, password)
   end
 end
 
 class Controller
+  def home
+    Views.login
+  end
 
+  def authenticate(username, password)
+    @gmail = GmailModel.new(username, password)
+  end
 end
 
 class GmailModel
+  def authenticate(user
+
+  end
 
 end
+
+controller = Controller.new
+controller.home
