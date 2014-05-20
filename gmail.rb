81af2c71036e8c02c94fd7e8bb6fc432aa1feb52
Matthew Bunday <zenceph@gmail.com>
2014-05-20 12:45:59 -0400

    Listing emails working.

--- gmail.rb
+++ gmail.rb
@@ -11,27 +11,71 @@ class Views
     password = HighLine.ask("Password: ") { |q| q.echo = '*' }
     @controller.authenticate(username, password)
   end
+
+  def mailbox(emails)
+    num = 1
+    emails.each do |mail|
+      puts "#{num.to_s.ljust(2)}. #{mail[:subject]}"
+      num += 1
+    end
+  end
 end
 
 class Controller
+  attr_accessor :gm
+
   def initialize
     @views = Views.new(self)
+    @gm = GmailModel.new
   end
 
-  def home
+  def mailbox
+    emails = @gm.latest_emails(20)
+    emails.map! {|email| {subject: email.subject}}
+    @views.mailbox(emails)
+  end
+
+  def login
     @views.login
   end
 
+  def home
+    if @gm.logged_in?
+      mailbox
+    else
+      login
+    end
+  end
+
   def authenticate(username, password)
-    @gm = GmailModel.new(username, password)
+    @gm.authenticate(username, password)
+    home
   end
 end
 
 class GmailModel
-  def initialize(username, password)
+  def initialize
+    @gmail = nil
+  end
+
+  def authenticate(username, password)
     @gmail = Gmail.new(username, password)
+    @gmail.login
+  end
+
+  def latest_emails(num)
+    @gmail.inbox.emails(:all).reverse.take(10)
+  end
+
+  def logged_in?
+    if @gmail.nil?
+      return false
+    else
+      return @gmail.logged_in?
+    end
   end
 end
 
 controller = Controller.new
+p controller.gm
 controller.home
