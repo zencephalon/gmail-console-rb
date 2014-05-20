452e98108628f723269aa1e72af1e97a3f75ed36
Matthew Bunday <zenceph@gmail.com>
2014-05-20 13:09:35 -0400

    View individual emails.

--- gmail.rb
+++ gmail.rb
@@ -19,8 +19,13 @@ class Views
       num += 1
     end
 
-    next_action = HighLine.ask("> or q: ")
+    next_action = HighLine.ask("> or q or #: ")
     @controller.mailbox(n, next_action)
+  end
+
+  def email(mail)
+    puts mail[:subject]
+    puts mail[:body]
   end
 end
 
@@ -32,6 +37,12 @@ class Controller
     @gm = GmailModel.new
   end
 
+  def email(n)
+    email = @gm.get_email(n)
+    email = {subject: email.subject, body: email.message.text_part.body}
+    @views.email(email)
+  end
+
   def mailbox(n = 0, action = nil)
     if action.nil?
       emails = @gm.latest_emails(20, n)
@@ -41,6 +52,8 @@ class Controller
       exit
     elsif action == '>'
       mailbox(n + 20)
+    else
+      email(action.to_i)
     end
   end
 
